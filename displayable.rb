e66a27fd19ad7864fdeab1f6bb5390ed5a4022e6
Vincent Klimjack <vklimjack@gmail.com>
2020-10-26 08:45:50 +0000

    added displayable module and guess prompt/validation methods

---
+++ displayable.rb
@@ -0,0 +1,9 @@
+module Displayable
+  def guess_prompt_message
+    puts 'Enter a letter! (a-z)'
+  end
+
+  def invalid_input_message
+    puts 'Please only enter letters (a-z).'
+  end
+end
--- game.rb
+++ game.rb
@@ -1,11 +1,17 @@
 require_relative 'player'
+require_relative 'displayable'
 
 class Game
+  include Displayable
   attr_reader :dictionary
 
   def initialize
     @dictionary = clean_dictionary(load_dictionary)
     @secret_word = choose_word
+  end
+
+  def play_game
+    prompt_for_guess
   end
 
   private
@@ -31,4 +37,21 @@ class Game
   def choose_word
     dictionary.sample
   end
+
+  def prompt_for_guess
+    guess_prompt_message
+    guess = gets.downcase.chomp
+    until valid_guess?(guess)
+      invalid_input_message
+      guess = gets.downcase.chomp
+    end
+    guess
+  end
+
+  def valid_guess?(guess)
+    guess.match?(/[a-z]/) && guess.length == 1
+  end
 end
+
+x = Game.new
+x.play_game
