d89e8b316505b6c4a355dd15e2cb554d939ab479
Vincent Klimjack <vklimjack@gmail.com>
2020-10-26 11:23:07 +0000

    added lots of displayable methods, as well as completing the game loop for a full game, no saves.

--- displayable.rb
+++ displayable.rb
@@ -1,9 +1,42 @@
 module Displayable
+  def instructions
+    puts <<~HEREDOC
+                              Welcome to Hangman!
+
+      The rules are simple - each round, you'll be prompted to guess a letter.
+      If that letter is correct, you'll see where that letter is in the secret word.
+      If it's wrong, you'll see your incorrect guesses go up by one.
+      If you can guess the secret word in less than 6 guesses, you win! Otherwise, well... 
+      
+                            R.I.P. Your hanging man.
+    HEREDOC
+  end
+
   def guess_prompt_message
     puts 'Enter a letter! (a-z)'
   end
 
   def invalid_input_message
-    puts 'Please only enter letters (a-z).'
+    puts 'Please only enter a single letter, (a-z).'
+  end
+
+  def show_guess_progress
+    puts current_guess_progress.join(' ')
+  end
+
+  def show_incorrect_guesses
+    puts "Incorrect guesses: #{incorrect_guesses.join(', ')}"
+  end
+
+  def show_incorrect_guess_count
+    puts "Incorrect guess count: #{incorrect_guess_count}"
+  end
+
+  def game_lost
+    puts "Game over! The secret word was \"#{secret_word.join}\"."
+  end
+
+  def game_won
+    puts 'Nice job! Hangman is forever in your debt.'
   end
 end
--- game.rb
+++ game.rb
@@ -3,18 +3,38 @@ require_relative 'displayable'
 
 class Game
   include Displayable
-  attr_reader :dictionary
+  attr_reader :dictionary, :incorrect_guesses, :incorrect_guess_count, :current_guess_progress
 
   def initialize
     @dictionary = clean_dictionary(load_dictionary)
-    @secret_word = choose_word
+    @secret_word = choose_word.chars
+    @current_guess_progress = secret_word_to_underscores
+    @incorrect_guess_count = 0
+    @incorrect_guesses = []
   end
 
   def play_game
-    prompt_for_guess
+    instructions
+    loop do
+      return game_lost if incorrect_guess_count == 6
+      return game_won if current_guess_progress == secret_word
+
+      guess = prompt_for_guess
+      if correct_guess?(guess)
+        update_current_guess_progress(guess)
+      else
+        update_incorrect_guesses(guess)
+        @incorrect_guess_count += 1
+      end
+      show_incorrect_guess_count
+      show_incorrect_guesses
+      show_guess_progress
+    end
   end
 
   private
+
+  attr_reader :secret_word
 
   def load_dictionary
     File.readlines('dictionary.txt', chomp: true)
@@ -38,6 +58,10 @@ class Game
     dictionary.sample
   end
 
+  def secret_word_to_underscores
+    secret_word.map { '_' }
+  end
+
   def prompt_for_guess
     guess_prompt_message
     guess = gets.downcase.chomp
@@ -50,6 +74,20 @@ class Game
 
   def valid_guess?(guess)
     guess.match?(/[a-z]/) && guess.length == 1
+  end
+
+  def correct_guess?(guess)
+    secret_word.include?(guess)
+  end
+
+  def update_current_guess_progress(guess)
+    secret_word.each_with_index do |letter, index|
+      current_guess_progress[index] = guess if letter == guess
+    end
+  end
+
+  def update_incorrect_guesses(guess)
+    incorrect_guesses << guess
   end
 end
 
